4835dbbc53fd5d092ed1503ac6c1e1eaae104350
Nuno Pereira <nunocgpereira@gmail.com>
2020-05-20 19:54:35 +0100

    style: add style to top show view

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.main')
 
 @section('content')
+    <div class="movie-info birder-b border-gray-800">
+        <div class="container mx-auto px-4 py-16 flex">
+            <div class="flex-none">
+                <img src="/imgs/parasite.jpg" alt="parasite" class="w-96">
+            </div>
+            <div class="ml-24">
+                <h2 class="text-4xl font-semibold">Parasite (2019)</h2>
+                <div class="flex items-center text-gray-400 text-sm">
+                    <span>s</span>
+                    <span class="ml-1 text-sm">75%</span>
+                    <span class="mx-2">|</span>
+                    <span>Feb 20, 2020</span>
+                    <span class="mx-2">|</span>
+                    <span>Action, Thriller, Comedy</span>
+                </div>
 
+                <p class="text-gray-300 mt-8">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo id, commodi unde deserunt nam odio tenetur quam vel laboriosam odit assumenda repellendus. Nobis iusto nam aspernatur est recusandae iure laudantium!
+                </p>
+
+                <div class="mt-12">
+                    <h4 class="text-white font-semibold">Featured Cats</h4>
+                    <div class="flex mt-4">
+                        <div>
+                            <div>Bong Joon-ho</div>
+                            <div class="text-sm text-gray-400">Sccreenplay, Director, Story</div>
+                        </div>
+                        <div class="ml-8">
+                            <div>Bong Joon-ho</div>
+                            <div class="text-sm text-gray-400">Sccreenplay, Director, Story</div>
+                        </div>
+                    </div>
+
+                    <div class="mt-12">
+                        <button class="flex items center bg-orange-500 text-gray-900 rounded font-semibold px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+                            <span class="ml-2">Play Trailer</span>
+                        </button>
+                    </div-mt-12>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
